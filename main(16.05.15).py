0a10fdd8c38c53bad9ca00062f4f7b93cd72cbfb
qndls42 <qndls42@naver.com>
2016-05-15 17:11:39 +0900

    URL주소로 xml파일 저장
    서비스키를 적용하여 url을 작성하고, 그 내용을 xml파일로 바탕화면에 저장

---
+++ main(16.05.15).py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import urllib
+
+url = "http://data.ex.co.kr/exopenapi/business/representFoodServiceArea?serviceKey=d%2Bgpbsvn3Vl5kqm1vaj9PqFxvOKhhGQ74xWyyrZG7803Gpkg%2BRsycotbu0JZ8sIVDT5jbpE2Ey5DzZdSXAVB2g%3D%3D&type=xml"
+
+data = urllib.request.urlopen(url).read()
+
+xmlFile = "test.xml"
+f = open(xmlFile, "wb")
+f.write(data)
+f.close()
